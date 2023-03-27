aacd7255d895ea0e183ae7452b1423ee44dc11c8
Garrido <giovanni.garrido228724@potros.itson.edu.mx>
2023-03-27 00:29:49 -0700

    menutest

--- app/src/main/java/mx/edu/itson/appmoviles/Splash.kt
+++ app/src/main/java/mx/edu/itson/appmoviles/Splash.kt
@@ -3,9 +3,9 @@ package mx.edu.itson.appmoviles
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Perfil : AppCompatActivity() {
+class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perfil)
+        setContentView(R.layout.activity_splash)
     }
 }
