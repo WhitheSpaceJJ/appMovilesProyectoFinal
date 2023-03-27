484b7845355e36a2498ccb4fafc476618930eb25
Garrido <giovanni.garrido228724@potros.itson.edu.mx>
2023-03-27 15:15:08 -0700

    resolve: "unresolved reference R"

--- app/src/androidTest/java/IniciarSesion.kt
+++ app/src/androidTest/java/IniciarSesion.kt
@@ -1,9 +1,11 @@
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mx.edu.itson.appmoviles.R
 
 class IniciarSesion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iniciar_sesion)
+
     }
 }
