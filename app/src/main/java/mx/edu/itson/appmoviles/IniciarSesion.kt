7e4052ee22970ce886a36eaaacf4dc9240050c91
WhiteSpaceJJ <jose.orozco229141@potros.itson.edu.mx>
2023-03-27 14:26:42 -0600

    pinshi sesion

--- app/src/main/java/mx/edu/itson/appmoviles/IniciarSesion.kt
+++ app/src/main/java/mx/edu/itson/appmoviles/IniciarSesion.kt
@@ -3,6 +3,7 @@ package mx.edu.itson.appmoviles
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class IniciarSesion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
