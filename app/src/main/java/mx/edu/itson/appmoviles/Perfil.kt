f29a5e3a6d0b49ad04826b4c1fd4805bb4403520
Garrido <giovanni.garrido228724@potros.itson.edu.mx>
2023-03-28 18:11:07 -0700

    perfil fragment

--- app/src/main/java/mx/edu/itson/appmoviles/Perfil.kt
+++ app/src/main/java/mx/edu/itson/appmoviles/Perfil.kt
@@ -1,11 +1,20 @@
 package mx.edu.itson.appmoviles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class Perfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
+
+        val imgConfiguracion: ImageView = findViewById(R.id.img_configuracion_perfil)
+
+        imgConfiguracion.setOnClickListener{
+            var intent: Intent = Intent(this, Configuracion::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/mx/edu/itson/appmoviles/Perfiles.kt
+++ app/src/main/java/mx/edu/itson/appmoviles/Perfiles.kt
@@ -1,11 +1,15 @@
 package mx.edu.itson.appmoviles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class Perfiles : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfiles)
+
+
     }
 }
