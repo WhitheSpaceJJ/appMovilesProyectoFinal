b5420e6f05598639321111fc096315651bcc4cbc
Garrido <giovanni.garrido228724@potros.itson.edu.mx>
2023-03-28 19:54:26 -0700

    fragmen perfil listen configuracion

--- app/src/main/java/mx/edu/itson/appmoviles/FragmentPerfil.kt
+++ app/src/main/java/mx/edu/itson/appmoviles/FragmentPerfil.kt
@@ -1,11 +1,13 @@
 package mx.edu.itson.appmoviles
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,7 +37,18 @@ class FragmentPerfil : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_perfil, container, false)
+
+        val ivConfiguracion: ImageView = view.findViewById(R.id.img_configuracion)
+
+        ivConfiguracion.setOnClickListener {
+            // Crea un Intent para llamar la Activity
+
+            val intent = Intent(activity, Configuracion::class.java)
+            startActivity(intent)
+
+        }
+        return view
     }
 
     companion object {
