45c1cf3578348d94fd163311802e79d0f767ffb2
itzelhgarza <itzel.garza216628@potros.itson.edu.mx>
2023-03-30 00:04:27 -0700

    Avance de diseño

--- app/src/main/java/mx/edu/itson/appmoviles/IniciarSesion.kt
+++ app/src/main/java/mx/edu/itson/appmoviles/IniciarSesion.kt
@@ -1,12 +1,35 @@
 package mx.edu.itson.appmoviles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 
 class IniciarSesion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iniciar_sesion)
+
+        val btnIniciarSesion: Button = findViewById(R.id.btn_iniciar_sesion)
+        val tvolvide_Pass: TextView = findViewById(R.id.tv_olvide_pass)
+        val btnRegistrase: Button = findViewById(R.id.btn_registrarse)
+
+        btnIniciarSesion.setOnClickListener {
+            var intent: Intent = Intent(this, Menu::class.java)
+            startActivity(intent)
+        }
+
+        tvolvide_Pass.setOnClickListener {
+            var intent: Intent = Intent(this, PaswordOlvidada::class.java)
+            startActivity(intent)
+        }
+
+        btnRegistrase.setOnClickListener {
+            var intent: Intent = Intent(this, CrearCuenta::class.java)
+            startActivity(intent)
+        }
+
     }
 }
