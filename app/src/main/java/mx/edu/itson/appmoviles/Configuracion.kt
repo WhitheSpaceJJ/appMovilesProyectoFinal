de54a4435206e23edd6843aafa97cb75ebf1d1cd
Garrido <giovanni.garrido228724@potros.itson.edu.mx>
2023-03-29 17:53:52 -0700

    faq

--- app/src/main/java/mx/edu/itson/appmoviles/Configuracion.kt
+++ app/src/main/java/mx/edu/itson/appmoviles/Configuracion.kt
@@ -1,11 +1,20 @@
 package mx.edu.itson.appmoviles
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class Configuracion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuracion)
+
+        val btn_ayuda: Button = findViewById(R.id.buttonAyuda)
+
+        btn_ayuda.setOnClickListener {
+            var intent: Intent = Intent(this, Faq::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/mx/edu/itson/appmoviles/Faq.kt
+++ app/src/main/java/mx/edu/itson/appmoviles/Faq.kt
@@ -1,11 +1,74 @@
 package mx.edu.itson.appmoviles
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 
-class Faq : AppCompatActivity() {
+class Faq : AppCompatActivity(), View.OnClickListener {
+
+    var tv_mini_cambio: TextView? =null
+    var tv_mini_olvide: TextView? =null
+    var tv_mini_sin_imagen: TextView? =null
+    var tv_mini_perfiles: TextView? =null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_faq)
+
+        tv_mini_perfiles = findViewById(R.id.minitextviewPerfiles)
+        tv_mini_cambio = findViewById(R.id.minitextviewCambio)
+        tv_mini_olvide = findViewById(R.id.minitextviewOlvide)
+        tv_mini_sin_imagen = findViewById(R.id.minitextviewSinImagenes)
     }
+
+    override fun onClick(view: View) {
+
+        when (view.id) {
+            R.id.buttonCantidadPerfiles -> {
+                if (tv_mini_perfiles?.isVisible == true) {
+                    tv_mini_perfiles?.setVisibility(View.GONE)
+                } else {
+                    tv_mini_perfiles?.setVisibility(View.VISIBLE)
+                }
+            }
+
+            R.id.buttonOlvideUsuario -> {
+                if (tv_mini_olvide?.isVisible == true) {
+                    tv_mini_olvide?.setVisibility(View.GONE)
+                } else {
+                    tv_mini_olvide?.setVisibility(View.VISIBLE)
+                }
+            }
+
+            R.id.buttonCambioIcono -> {
+                if (tv_mini_cambio?.isVisible == true) {
+                    tv_mini_cambio?.setVisibility(View.GONE)
+                } else {
+                    tv_mini_cambio?.setVisibility(View.VISIBLE)
+                }
+            }
+
+            R.id.buttonSinImagenes -> {
+                if (tv_mini_sin_imagen?.isVisible == true) {
+                    tv_mini_sin_imagen?.setVisibility(View.GONE)
+                } else {
+                    tv_mini_sin_imagen?.setVisibility(View.VISIBLE)
+                }
+            }
+        }
+    }
+
+
 }
+
+/*
+        btn_cantidad_perfiles.setOnClickListener {
+
+            tv_mini_perfiles.setVisibility(View.VISIBLE)
+        }
+ */
+
+
