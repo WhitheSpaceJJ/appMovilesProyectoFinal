c070c827897425693ddec97779981088cbdee52b
itzelhgarza <itzel.garza216628@potros.itson.edu.mx>
2023-03-30 01:38:10 -0700

    Inicio diseño perfiles

--- app/src/main/java/mx/edu/itson/appmoviles/Temas.kt
+++ app/src/main/java/mx/edu/itson/appmoviles/Temas.kt
@@ -1,11 +1,20 @@
 package mx.edu.itson.appmoviles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Temas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_temas)
+
+        val btn_listo : Button = findViewById(R.id.btn_listo)
+
+        btn_listo.setOnClickListener{
+            var intent: Intent = Intent(this, Menu::class.java)
+            startActivity(intent)
+        }
     }
 }
