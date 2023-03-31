93144cb8ab6b6fc36e387030865891b6168c0080
Garrido <giovanni.garrido228724@potros.itson.edu.mx>
2023-03-31 01:29:19 -0700

    navegacion

--- app/src/main/java/mx/edu/itson/appmoviles/Actividad.kt
+++ app/src/main/java/mx/edu/itson/appmoviles/Actividad.kt
@@ -1,11 +1,21 @@
 package mx.edu.itson.appmoviles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class Actividad : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actividad)
+
+        val iv_cerrar_actividad: ImageView = findViewById(R.id.ivCerrarActividad)
+
+        iv_cerrar_actividad.setOnClickListener {
+            var intent: Intent = Intent(this,Menu::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/mx/edu/itson/appmoviles/FinLectura.kt
+++ app/src/main/java/mx/edu/itson/appmoviles/FinLectura.kt
@@ -1,11 +1,27 @@
 package mx.edu.itson.appmoviles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FinLectura : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fin_lectura)
+
+        val btn_estoy_listo: Button = findViewById(R.id.btnestoyListo)
+
+        btn_estoy_listo.setOnClickListener {
+            var intent: Intent = Intent(this,Actividad::class.java)
+            startActivity(intent)
+        }
+
+        val btn_ahora_no: Button = findViewById(R.id.btnAhoraNo)
+
+        btn_ahora_no.setOnClickListener {
+            var intent: Intent = Intent(this,Menu::class.java)
+            startActivity(intent)
+        }
     }
 }
