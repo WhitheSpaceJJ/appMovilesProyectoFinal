65fc0940dc226f97ef4cdfa52448b4a51ff7fe6d
Garrido <giovanni.garrido228724@potros.itson.edu.mx>
2023-03-31 00:39:56 -0700

    miedo

---
+++ app/src/main/java/mx/edu/itson/appmoviles/PopUpDiccionario.kt
@@ -0,0 +1,21 @@
+package mx.edu.itson.appmoviles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.DisplayMetrics
+
+class PopUpPrueba : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pop_up_diccionario)
+
+        val medidasVentana: DisplayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(medidasVentana);
+
+        var ancho: Int = medidasVentana.widthPixels;
+        var alto: Int = medidasVentana.heightPixels
+
+        window.setLayout((ancho*0.75).toInt(), (alto*0.35).toInt())
+
+    }
+}
