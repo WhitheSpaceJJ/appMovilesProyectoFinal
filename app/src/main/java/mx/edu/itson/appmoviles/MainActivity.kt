b8c13f930cbf85cbabf90a9e02077d2d01fd6d75
WhiteSpaceJJ <jose.orozco229141@potros.itson.edu.mx>
2023-04-13 13:54:32 -0600

    temporizador main activity

--- app/src/main/java/mx/edu/itson/appmoviles/MainActivity.kt
+++ app/src/main/java/mx/edu/itson/appmoviles/MainActivity.kt
@@ -1,23 +1,23 @@
 package mx.edu.itson.appmoviles
 
 import android.content.Intent
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    private val DELAY_TIME = 3000L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imgLogo: ImageView = findViewById(R.id.img_logo_main)
-
-        imgLogo.setOnClickListener{
-            var intent: Intent = Intent(this, IniciarSesion::class.java)
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, IniciarSesion::class.java)
             startActivity(intent)
-        }
-
+            finish()
+        }, DELAY_TIME)
     }
 }
