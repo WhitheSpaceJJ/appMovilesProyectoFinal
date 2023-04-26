083db29bf62ed8b202e65f36626545a4ae8a5fae
Garrido <giovanni.garrido228724@potros.itson.edu.mx>
2023-04-25 21:09:46 -0700

    valida si el correo ya fue registrado, manda a iniciar sesion despues de registrar

--- app/src/main/java/mx/edu/itson/appmoviles/CrearCuenta.kt
+++ app/src/main/java/mx/edu/itson/appmoviles/CrearCuenta.kt
@@ -1,17 +1,23 @@
 package mx.edu.itson.appmoviles
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Printer
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.*
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class CrearCuenta : AppCompatActivity() {
+
+    var et_crear_usuario: EditText? = null
+    var et_crear_correo: EditText? = null
+    var et_crear_contra: EditText? = null
+    var et_crear_contra_confirmar: EditText? = null
 
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,41 +26,43 @@ class CrearCuenta : AppCompatActivity() {
 
         auth = Firebase.auth
 
-        val ivRegresar : ImageView = findViewById(R.id.ivRegresarCrear)
-        val btnRegistrase : Button = findViewById(R.id.btnCrearRegistrarse)
+        val ivRegresar: ImageView = findViewById(R.id.ivRegresarCrear)
+        val btnRegistrase: Button = findViewById(R.id.btnCrearRegistrarse)
 
-        ivRegresar.setOnClickListener{
+        ivRegresar.setOnClickListener {
             var intent: Intent = Intent(this, IniciarSesion::class.java)
             startActivity(intent)
         }
 
-        btnRegistrase.setOnClickListener{
+        btnRegistrase.setOnClickListener {
             //var intent: Intent = Intent(this, ConfigurarPerfil::class.java)
             //startActivity(intent)
             valida_registro()
         }
     }
 
-    private fun valida_registro(){
-        val et_crear_usuario : EditText = findViewById(R.id.etCrearusuario)
-        val et_crear_correo : EditText = findViewById(R.id.etCrearCorreoE)
-        val et_crear_contra : EditText = findViewById(R.id.etCrearContrasenia)
-        val et_crear_contra_confirmar: EditText = findViewById(R.id.etCrearContraseniaConfirmar)
+    private fun valida_registro() {
 
-        var usuario: String = et_crear_usuario.text.toString()
-        var correo: String = et_crear_correo.text.toString()
-        var contra1: String = et_crear_contra.text.toString()
-        var contra2: String = et_crear_contra_confirmar.text.toString()
+        et_crear_usuario = findViewById(R.id.etCrearusuario)
+        et_crear_correo= findViewById(R.id.etCrearCorreoE)
+        et_crear_contra= findViewById(R.id.etCrearContrasenia)
+        et_crear_contra_confirmar = findViewById(R.id.etCrearContraseniaConfirmar)
+
+        var usuario: String = et_crear_usuario?.text.toString()
+        var correo: String = et_crear_correo?.text.toString()
+        var contra1: String = et_crear_contra?.text.toString()
+        var contra2: String = et_crear_contra_confirmar?.text.toString()
 
         if (!usuario.isNullOrBlank() && !correo.isNullOrBlank() && !contra1.isNullOrBlank() &&
-            !contra2.isNullOrBlank()){
-            if(contra1 == contra2){
+            !contra2.isNullOrBlank()
+        ) {
+            if (contra1 == contra2) {
                 registrarFirebase(correo, contra1)
-            }else{
-                Toast.makeText(this,"Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
             }
-        }else{
-            Toast.makeText(this,"Por favor completa todos los campos", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Por favor completa todos los campos", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -66,17 +74,38 @@ class CrearCuenta : AppCompatActivity() {
                     //Log.d(TAG, "createUserWithEmail:success")
                     val user = auth.currentUser
 
-                    Toast.makeText(baseContext, "${user?.email} Se ha creado correctamente.",
-                        Toast.LENGTH_SHORT).show()
+
+                    Toast.makeText(
+                        baseContext, "${user?.email} Se ha creado correctamente, puede inciar sesión.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    limpiarCampos()
+                    val intent: Intent = Intent(this, IniciarSesion::class.java)
+                    startActivity(intent)
                     //updateUI(user)
+                } else if (task.exception.toString() == "com.google.firebase.auth.FirebaseAuthUserCollisionException: The email address is already in use by another account.") {
+
+                    Toast.makeText(
+                        baseContext, "Correo ya registrado",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+
                 } else {
-                    // If sign in fails, display a message to the user.
-                    //Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
-                    //updateUI(null)
+
+                    Toast.makeText(
+                        baseContext, task.exception.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
 
+    }
+
+    private fun limpiarCampos(){
+        et_crear_contra?.setText("")
+        et_crear_usuario?.setText("")
+        et_crear_correo?.setText("")
+        et_crear_contra_confirmar?.setText("")
     }
 }
