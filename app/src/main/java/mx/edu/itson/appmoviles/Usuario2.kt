ad20c8a06f1a951617bbc099b2220e0c0d7a66a9
Garrido <giovanni.garrido228724@potros.itson.edu.mx>
2023-05-12 11:58:46 -0700

    DEL EXAMEN

---
+++ app/src/main/java/mx/edu/itson/appmoviles/Usuario2.kt
@@ -0,0 +1,14 @@
+package mx.edu.itson.appmoviles
+
+import java.io.Serializable
+
+data class Usuario(
+    var uid: String?=null,
+    var correoElectronico: String?=null,
+    var nombreUsuario: String?=null,
+    var perfiles: ArrayList<PerfilUsuario> = ArrayList()
+
+
+): Serializable{
+    override fun toString() = correoElectronico + "\t" + nombreUsuario
+}
